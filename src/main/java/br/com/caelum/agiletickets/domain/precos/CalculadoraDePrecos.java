2e4ae610966fe37ebee141bf81df93e52b59461b
canaveral1208 <canaveral1208@gmail.com>
2014-12-10 12:28:33 -0200

    Refatoração do código da classe CalculadoraDePrecos.java

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -80,6 +80,9 @@ public class CalculadoraDePrecos {
 				preco = sessao.getPreco();
 		}
 		
+		// Implementando o Design Pattern Strategy.
+		// sessao.getTipo.getCalculador().calcula();
+		
 		return preco.multiply(BigDecimal.valueOf(quantidade));
 	}
 
